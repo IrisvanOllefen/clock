6ec85428323579af2a582ff655b9c6ad37bc525b
Iris van Ollefen <irisvanollefen@gmail.com>
2021-11-18 15:46:24 +0100

    Added basic information

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Clock</h1>
+      <h2>What is the time?</h2>
+      <h3>This is where the time will go</h3>
+    </div>
+  )
+}
+
+export default HomePage
