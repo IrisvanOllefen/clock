b4650b3190933a3d2a2d296ed5f7267e7ff2da64
Iris van Ollefen <irisvanollefen@gmail.com>
2021-11-19 11:01:47 +0100

    Added font styling to text

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout() {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Faster+One&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Faster+One&family=Righteous&display=swap" rel="stylesheet" />
             </Head>
         </div>      
     )
