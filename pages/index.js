fe172ef30006bc42f8d28b356f4a476ac04d1ac8
Iris van Ollefen <irisvanollefen@gmail.com>
2021-11-19 09:32:28 +0100

    Added styling

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import Layout from '/components/Layout'
+
 function HomePage() {
   return (
-    <div className="container">
-      <h1 className="title">Clock</h1>
-      <h2 className="question">What is the time?</h2>
-      <h3 className="time">This is where the time will go</h3>
-    </div>
+      <div className="wrapper">
+        <Layout />
+        <div className="container">
+          <h1 className="title">CLOCK</h1>
+          <h2 className="question">What is the time?</h2>
+          <h3 className="time">This is where the time will go</h3>
+        </div>
+      </div>
   )
 }
 
