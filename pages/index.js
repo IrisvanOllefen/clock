f270770d0bca81b4b4141d92b8d1b59c315ae5c0
Iris van Ollefen <irisvanollefen@gmail.com>
2021-11-19 10:52:23 +0100

    Made time component, clock works

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Layout from '/components/Layout'
+import Time from '/components/time'
 
-function HomePage() {
+function HomePage() {  
+
   return (
       <div className="wrapper">
         <Layout />
         <div className="container">
           <h1 className="title">CLOCK</h1>
           <h2 className="question">What is the time?</h2>
-          <h3 className="time">This is where the time will go</h3>
+          <h3 className="time"><Time /></h3>
         </div>
       </div>
   )
